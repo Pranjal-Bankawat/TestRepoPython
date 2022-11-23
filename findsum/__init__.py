67f2ac213f8eb3c1a0f1027351dc043309a655ff
Pranjal Bankawat <pranjal.bankawat@sap.com>
2022-11-23 14:20:32 +0530

    finally done

--- findsum/__init__.py
+++ findsum/__init__.py
@@ -2,7 +2,7 @@ def sum(arg):
     total = 0
     for val in arg:
         total += val
-    print("Now I will return the total!!")
+    print("Now I will return the total!!!")
     return total
 
 sum([1,2,3,4])
